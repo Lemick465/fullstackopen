7cfec6ee944bad64a975bb9fecd60f4dbcec1999
Lemick465 <lemickanfobotwe@gmail.com>
2025-07-08 19:28:21 +0000

    Exercise1.5: Made course and parts as one object

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -26,31 +26,33 @@ const Footer = (props) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
+      <Header course={course.name} />
       <Content
-        title1={parts[0].name} exercises1={parts[0].exercises}
-        title2={parts[1].name} exercises2={parts[1].exercises}
-        title3={parts[2].name} exercises3={parts[2].exercises}
+        title1={course.parts[0].name} exercises1={course.parts[0].exercises}
+        title2={course.parts[1].name} exercises2={course.parts[1].exercises}
+        title3={course.parts[2].name} exercises3={course.parts[2].exercises}
       />
-      <Footer total={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+      <Footer total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
     </div>
   );
 };
