a4970c066fd252e116460b89b3e8a270e0639c67
Lemick465 <lemickanfobotwe@gmail.com>
2025-07-11 10:20:16 +0000

    Exercise1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+
+const Title = ({text}) => <h1>{text}</h1>
+
+const Statistics = (props) => {
+  return(
+    <div>
+      <Title text={props.text} />
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+    </div>
+  )
+}
+
+const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
+
+const App = () => {
+  const [statistics, setStatistics] = useState({good: 0, neutral: 0, bad: 0})
+
+  const handleOnGoodClick = () => {
+    const updatedGood = {
+      ...statistics,
+      good: statistics.good + 1
+    }
+    setStatistics(updatedGood)
+  }
+
+    const handleOnNeutralClick = () => {
+    const updatedNeutral = {
+      ...statistics,
+      neutral: statistics.neutral + 1
+    }
+    setStatistics(updatedNeutral)
+  }
+
+    const handleOnBadClick = () => {
+    const updatedBad = {
+      ...statistics,
+      bad: statistics.bad + 1
+    }
+    setStatistics(updatedBad)
+  }
+
+  return(
+    <div>
+      <Title text="give feedback" />
+      <Button text="good" onClick={handleOnGoodClick} />
+      <Button text="neutral" onClick={handleOnNeutralClick} />
+      <Button text="bad" onClick={handleOnBadClick} />
+      <Statistics text="Statistics"
+        good={statistics.good}
+        neutral={statistics.neutral}
+        bad={statistics.bad} />
+    </div>
+  )
+}
+
+export default App
