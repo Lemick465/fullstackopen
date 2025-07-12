a4f9308c413208c77d756eab1ef176c7c65e0b96
Lemick465 <lemickanfobotwe@gmail.com>
2025-07-12 13:50:50 +0000

    unicafe: Exercise1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,75 +2,81 @@ import { useState } from "react";
 
 const Title = ({ text }) => <h1>{text}</h1>;
 
-const Statistics = ({ title, good, neutral, bad }) => {
-  const total = good + neutral + bad;
-
-  const average = (good - bad) / total;
-  const positivePercentage = (good / total) * 100;
-
-  if(total === 0){
-   return(
-    <div>
-      <Title text={title} />
-      <p>No feedback given</p>
-    </div>
-   )
-  }
-
-  return (
-    <div>
-      <Title text={title} />
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positivePercentage}%</p>
-    </div>
-  );
-};
+const StatisticLine = ({ text, value }) => <div>{text} {value}</div>
 
 const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>;
 
 const App = () => {
-  const [statistics, setStatistics] = useState({ good: 0, neutral: 0, bad: 0 });
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [posPercentage, setPosPercentage] = useState(0)
 
   const handleOnGoodClick = () => {
-    const updatedGood = {
-      ...statistics,
-      good: statistics.good + 1,
-    };
-    setStatistics(updatedGood);
+    const updatedGood = good + 1
+    const updatedTotal = total + 1
+    const updatedAverage = (updatedGood - bad) / updatedTotal
+    const updatedPositive = (updatedGood / updatedTotal) * 100
+
+    setGood(updatedGood)
+    setTotal(updatedTotal)
+    setAverage(updatedAverage)
+    setPosPercentage(updatedPositive)
+
   };
 
-  const handleOnNeutralClick = () => {
-    const updatedNeutral = {
-      ...statistics,
-      neutral: statistics.neutral + 1,
-    };
-    setStatistics(updatedNeutral);
+    const handleOnNeutralClick = () => {
+    const updatedNeutral = neutral + 1
+    const updatedTotal = total + 1
+    const updatedAverage = (good - bad) / updatedTotal
+    const updatedPositive = (good / updatedTotal) * 100
+
+    setNeutral(updatedNeutral)
+    setTotal(updatedTotal)
+    setAverage(updatedAverage)
+    setPosPercentage(updatedPositive)
   };
 
   const handleOnBadClick = () => {
-    const updatedBad = {
-      ...statistics,
-      bad: statistics.bad + 1,
-    };
-    setStatistics(updatedBad);
+    const updatedBad = bad + 1
+    const updatedTotal = total + 1
+    const updatedAverage = (good - updatedBad) / updatedTotal
+    const updatedPositive = (good / updatedTotal) * 100
+
+    setBad(updatedBad)
+    setTotal(updatedTotal)
+    setAverage(updatedAverage)
+    setPosPercentage(updatedPositive)
   };
+
+  if(total === 0){
+    return(
+      <div>
+      <Title text="give feedback" />
+      <Button text="good" onClick={handleOnGoodClick}/>
+      <Button text="neutral" onClick={handleOnNeutralClick}/>
+      <Button text="bad" onClick={handleOnBadClick}/>
+      <Title text="statistics" />
+      <p>No feedback available</p>
+      </div>
+    )
+  }
 
   return (
     <div>
       <Title text="give feedback" />
-      <Button text="good" onClick={handleOnGoodClick} />
-      <Button text="neutral" onClick={handleOnNeutralClick} />
-      <Button text="bad" onClick={handleOnBadClick} />
-      <Statistics
-        title="Statistics"
-        good={statistics.good}
-        neutral={statistics.neutral}
-        bad={statistics.bad}
-      />
+      <Button text="good" onClick={handleOnGoodClick}/>
+      <Button text="neutral" onClick={handleOnNeutralClick}/>
+      <Button text="bad" onClick={handleOnBadClick}/>
+      <Title text="statistics" />
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={total} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={posPercentage} />
     </div>
   );
 };
