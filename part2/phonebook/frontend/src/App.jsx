1d47b0e873f0a195d4446bd643af2c90707ef727
Lemick465 <lemickanfobotwe@gmail.com>
2025-07-17 15:45:34 +0000

    phonebook: Exercise2.9: Implement search functionality

--- part2/phonebook/frontend/src/App.jsx
+++ part2/phonebook/frontend/src/App.jsx
@@ -1,56 +1,88 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: "39-44-53523" }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const searchResult = persons.filter((person) =>
+    person.name.toLowerCase().includes(search.trim().toLowerCase())
+  );
 
   const handleAddPerson = (event) => {
-    event.preventDefault()
-    const nameExists = persons.some((person) => person.name === newName)
-    if(nameExists){
-      return alert(`${newName} is already added to phonebook`)
+    event.preventDefault();
+    const nameExists = persons.some((person) => person.name.trim().toLowerCase() === newName.trim().toLowerCase());
+    if (nameExists) {
+      return alert(`${newName} is already added to phonebook`);
+    } else {
+      const newPerson = {
+        name: newName,
+        number: newNumber,
+        id:
+          persons.length > 0
+            ? Math.max(...persons.map((person) => person.id)) + 1
+            : 1,
+      };
+      setPersons(persons.concat(newPerson));
+      setNewName("");
+      setNewNumber("");
     }
-    else{
-    const newPerson = {
-      name: newName,
-      number: newNumber,
-    }
-    setPersons(persons.concat(newPerson))
-    setNewName("")
-    setNewNumber("")
-    }
-  }
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input onChange={handleSearchChange} value={search} />
+      </div>
+      <div>
+        {search.trim() !== "" &&
+          searchResult.map((result) => (
+            <div key={result.id}>
+              {result.name} {result.number}
+            </div>
+          ))}
+      </div>
+
+      <h2>add a new number</h2>
       <form onSubmit={handleAddPerson}>
         <div>
-          name: <input onChange={handleNameChange} value={newName}/>
+          name: <input onChange={handleNameChange} value={newName} />
         </div>
         <div>
-          number: <input onChange={handleNumberChange} value={newNumber}/>
+          number: <input onChange={handleNumberChange} value={newNumber} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, index) =>
-      <div key={index}>{person.name} {person.number}</div>)}
+      {persons.map((person) => (
+        <div key={person.id}>
+          {person.name} {person.number}
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
