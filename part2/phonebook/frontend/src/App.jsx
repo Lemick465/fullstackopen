86eb70c10a866393e2f5f8701de99f72255bc89a
Lemick465 <lemickanfobotwe@gmail.com>
2025-07-17 10:24:37 +0000

    phonebook: Exercise2.8: Allow users to add numbers

--- part2/phonebook/frontend/src/App.jsx
+++ part2/phonebook/frontend/src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: "39-44-53523" }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const handleAddPerson = (event) => {
@@ -19,9 +24,11 @@ const App = () => {
     else{
     const newPerson = {
       name: newName,
+      number: newNumber,
     }
     setPersons(persons.concat(newPerson))
     setNewName("")
+    setNewNumber("")
     }
   }
 
@@ -33,12 +40,15 @@ const App = () => {
           name: <input onChange={handleNameChange} value={newName}/>
         </div>
         <div>
+          number: <input onChange={handleNumberChange} value={newNumber}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person, index) =>
-      <div key={index}>{person.name}</div>)}
+      <div key={index}>{person.name} {person.number}</div>)}
     </div>
   )
 }
