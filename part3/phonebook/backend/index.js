20e4ee7bc56066056a4cc81a8731b07f19de1b7d
Lemick465 <lemickanfobotwe@gmail.com>
2025-08-16 18:11:30 +0000

    phonebook-backend: Exercise3.4: API endpoint to delete a person

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -45,6 +45,19 @@ app.get("/api/persons/:id", (request, response) => {
     response.json(person)
 })
 
+app.delete("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find((person) => person.id === id)
+    if(!person){
+        response.status(404).json({
+            error: "Person not found!"
+        })
+    }
+    const newPersons = persons.filter((person) => person.id !== id)
+    persons = newPersons
+    response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
