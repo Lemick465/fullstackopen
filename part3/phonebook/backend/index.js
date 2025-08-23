423e1f6cc0b5061da7fb95319c9456d694014e44
Lemick465 <lemickanfobotwe@gmail.com>
2025-08-23 21:51:27 +0000

    Custom request logger middleware

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -12,9 +12,19 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
+const requestLogger = (request, response, next) => {
+  console.log("Method", request.method);
+  console.log("Path", request.path);
+  console.log("Body", request.body);
+  console.log("---");
+  next();
+};
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
+
+app.use(requestLogger);
 
 const generateId = () => {
   const maxId =
