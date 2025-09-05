fdbda66d5beeb67eb6652db53ab3fc16f5840d43
Lemick465 <lemickanfobotwe@gmail.com>
2025-09-05 12:27:20 +0000

    phonebook: Exercises3.9-3.11: Integrate frontend with backend API

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,10 +1,15 @@
 const express = require("express")
 const app = express()
 const morgan = require("morgan")
+const cors = require("cors")
 
 morgan.token("body", request => {
     return JSON.stringify(request.body)
 })
+
+app.use(cors())
+
+app.use(express.static("dist"))
 
 app.use(express.json())
 
@@ -96,7 +101,7 @@ app.post("/api/persons", (request, response) => {
     response.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
