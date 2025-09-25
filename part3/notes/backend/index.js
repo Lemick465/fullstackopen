355a055353190b644bfe970068e69d42b11a4037
Lemick465 <lemickanfobotwe@gmail.com>
2025-09-25 10:24:47 +0000

    Retrieve data from database

--- part3/notes/backend/index.js
+++ part3/notes/backend/index.js
@@ -9,14 +9,6 @@ morgan.token("body", request => {
   return JSON.stringify(request.body)
 })
 
-app.use(cors())
-
-app.use(express.json())
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-app.use(express.static("dist"))
-
 const requestLogger = (request, response, next) => {
   console.log("Method", request.method)
   console.log("Path", request.path)
@@ -29,52 +21,22 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" })
 }
 
+app.use(cors())
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static("dist"))
 app.use(requestLogger)
 
-generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map((note) => Number(note.id)))
-    : 0
-  return String(maxId + 1)
-}
-
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
-app.get("/", (request, response) => {
-    response.send("<h1>Hello World!</h1>")
-})
-
 app.get("/api/notes", (request, response) => {
-    // response.json(notes)
     Note.find({}).then((notes) => {
       response.json(notes)
     })
 })
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = request.params.id
-  const note = notes.find((note) => note.id === id)
-  if(note){
+  Note.findById(request.params.id).then((note) => {
     response.json(note)
-  }else{
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -93,13 +55,14 @@ app.post("/api/notes", (request, response) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
+  note.save().then((savedNote) => {
+    response.json(savedNote)
+  })
 
   response.json(note)
 })
