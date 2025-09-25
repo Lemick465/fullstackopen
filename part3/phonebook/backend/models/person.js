d2baf6b7feae5f2603d5290625ffd181c713d7fc
Lemick465 <lemickanfobotwe@gmail.com>
2025-09-25 10:56:15 +0000

    phonebook: Exercise3.13: Connecting backend to database

---
+++ part3/phonebook/backend/models/person.js
@@ -0,0 +1,28 @@
+require("dotenv").config()
+const mongoose = require("mongoose")
+
+const url = process.env.MONGODB_URI
+
+mongoose
+    .connect(url)
+    .then((result) => {
+        console.log("connected to MongoDB", url)
+    })
+    .catch((error) => {
+        console.log("error connecting to MongoDB", error.message)
+    })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model("Person", personSchema)
