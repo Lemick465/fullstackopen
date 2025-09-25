6ac1c2d496931f34f3fb3a7a37925cf3ecd87c04
Lemick465 <lemickanfobotwe@gmail.com>
2025-09-25 10:56:58 +0000

    Fetching data from database

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -2,17 +2,15 @@ const express = require("express")
 const app = express()
 const morgan = require("morgan")
 const cors = require("cors")
+const Person = require("./models/person")
 
 morgan.token("body", request => {
     return JSON.stringify(request.body)
 })
 
 app.use(cors())
-
 app.use(express.static("dist"))
-
 app.use(express.json())
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 const generateId = () => {
@@ -22,46 +20,21 @@ const generateId = () => {
     return (maxId + 1)
 }
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456",
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345",
-    },
-    {
-        id: 4,
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-    }
-  ]
 
 app.get("/info", (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${Date().toLocaleString()}</p>`)
 })
 
 app.get("/api/persons", (request, response) => {
-    response.json(persons)
+    Person.find({}).then((persons) => {
+        response.json(persons)
+    })
 })
 
 app.get("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find((person) => person.id === id)
-    if(!person){
-        response.status(404).json({
-            error: "Person not found!"
-        })
-    }
-    response.json(person)
+    Person.findById(request.params.id).then((person) => {
+        response.json(person)
+    })
 })
 
 app.delete("/api/persons/:id", (request, response) => {
