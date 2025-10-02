b50e2d4877a6b8694f627e11c2d3192714c55a0f
Lemick465 <lemickanfobotwe@gmail.com>
2025-10-02 16:55:16 +0000

    Refactoring the route responsible for fetching a single note

--- notes/backend/tests/note_api.test.js
+++ notes/backend/tests/note_api.test.js
@@ -8,11 +8,6 @@ const app = require('../app')
 
 const api = supertest(app)
 
-// const initialNotes = [
-//   { content: 'HTML is easy', important: false },
-//   { content: 'Browser can execute only JavaScript', important: true },
-// ]
-
 beforeEach(async () => {
   await Note.deleteMany({})
   let noteObject = new Note(helper.initialNotes[0])
@@ -51,6 +46,19 @@ test('note without content is not added', async () => {
 
   const notesAtEnd = await helper.notesInDb()
   assert.strictEqual(notesAtEnd.length, helper.initialNotes.length)
+})
+
+test('a specific note can be viewed', async () => {
+  const notesAtStart = await helper.notesInDb()
+  const noteToView = notesAtStart[0]
+
+
+  const resultNote = await api
+    .get(`/api/notes/${noteToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.deepStrictEqual(resultNote.body, noteToView)
 })
 
 test('notes are returned as json', async () => {
