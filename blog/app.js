e5fa04caa3e58051320afced5a24d59fd85f957e
Lemick465 <lemickanfobotwe@gmail.com>
2025-10-04 02:12:15 +0000

    Fix order of middlewares

--- blog/app.js
+++ blog/app.js
@@ -12,9 +12,9 @@ mongoose
   .then(() => logger.info('connected to MongoDB'))
   .catch((error) => logger.error(error.message))
 
+app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogRouter)
-app.use(express.json())
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
