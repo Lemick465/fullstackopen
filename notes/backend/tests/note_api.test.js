8ff2138ca47b3a0e2074a1259be3ab6cc0a6a90b
Lemick465 <lemickanfobotwe@gmail.com>
2025-10-04 03:38:19 +0000

    Unit test to verify successful user creation and duplicate usernames

--- notes/backend/tests/note_api.test.js
+++ notes/backend/tests/note_api.test.js
@@ -2,9 +2,11 @@ const assert = require('node:assert')
 const { test, after, beforeEach, describe } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const bcrypt = require('bcrypt')
 const app = require('../app')
 const helper = require('./test_helper')
 const Note = require('../models/note')
+const User = require('../models/user')
 
 const api = supertest(app)
 
@@ -50,17 +52,13 @@ describe('when there is initially some notes saved', () => {
     test('fails with statuscode 404 if note does not exist', async () => {
       const validNonexistingId = await helper.nonExistingId()
 
-      await api
-        .get(`/api/notes/${validNonexistingId}`)
-        .expect(404)
+      await api.get(`/api/notes/${validNonexistingId}`).expect(404)
     })
 
     test('fails with statuscode 400 id is invalid', async () => {
       const invalidId = '5a3d5da59070081a82a3445'
 
-      await api
-        .get(`/api/notes/${invalidId}`)
-        .expect(400)
+      await api.get(`/api/notes/${invalidId}`).expect(400)
     })
   })
 
@@ -87,10 +85,7 @@ describe('when there is initially some notes saved', () => {
     test('fails with status code 400 if data invalid', async () => {
       const newNote = { important: true }
 
-      await api
-        .post('/api/notes')
-        .send(newNote)
-        .expect(400)
+      await api.post('/api/notes').send(newNote).expect(400)
 
       const notesAtEnd = await helper.notesInDb()
 
@@ -103,9 +98,7 @@ describe('when there is initially some notes saved', () => {
       const notesAtStart = await helper.notesInDb()
       const noteToDelete = notesAtStart[0]
 
-      await api
-        .delete(`/api/notes/${noteToDelete.id}`)
-        .expect(204)
+      await api.delete(`/api/notes/${noteToDelete.id}`).expect(204)
 
       const notesAtEnd = await helper.notesInDb()
 
@@ -114,6 +107,56 @@ describe('when there is initially some notes saved', () => {
 
       assert.strictEqual(notesAtEnd.length, helper.initialNotes.length - 1)
     })
+  })
+})
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = helper.usersInDb()
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map((user) => user.username)
+    assert(usernames.includes(newUser.username))
+  })
+
+  test('creation fails with proper status code and message if username is already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert(result.body.error.includes('expected `username` to be unique'))
+    assert(usersAtStart.length, usersAtEnd.length)
   })
 })
 
