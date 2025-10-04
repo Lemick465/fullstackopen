ed9644458304549302fa82e9dfa150a6c68648ad
Lemick465 <lemickanfobotwe@gmail.com>
2025-10-04 02:13:04 +0000

    Route to update blog

--- blog/controller/blogs.js
+++ blog/controller/blogs.js
@@ -21,4 +21,20 @@ blogRouter.delete('/:id', async (request, response) => {
   response.status(204).end()
 })
 
+blogRouter.put('/:id', async (request, response) => {
+  const { title, author, url, likes } = request.body
+  const blog = await Blog.findById(request.params.id)
+  if (!blog) {
+    return response.status(404).end()
+  }
+
+  blog.title = title
+  blog.author = author
+  blog.url = url
+  blog.likes = likes
+
+  const updatedBlog = await blog.save()
+  response.status(200).json(updatedBlog)
+})
+
 module.exports = blogRouter
