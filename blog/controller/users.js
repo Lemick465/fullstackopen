28995c43d809972e44d10ce44f7f06f5b5120184
Lemick465 <lemickanfobotwe@gmail.com>
2025-10-06 01:24:19 +0000

    Validations to prevent creation of invalid users

--- blog/controller/users.js
+++ blog/controller/users.js
@@ -10,16 +10,31 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
+  if (!username || !password) {
+    return response
+      .status(400)
+      .json({ error: 'username and password are required' })
+  }
+
+  if (password.length < 3) {
+    return response.status(400).json({ error: 'password is too short' })
+  }
+
   const salt = 10
   const passwordHash = await bcrypt.hash(password, salt)
 
   const userObject = new User({
     username,
     name,
-    passwordHash
+    passwordHash,
   })
 
   const savedUser = await userObject.save()
+
+  // Ensuring hash is not leaked
+  const safeUser = savedUser.toJSON()
+  delete safeUser.passwordHash
+
   response.status(201).json(savedUser)
 })
 
